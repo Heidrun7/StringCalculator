d746ab48ccebac69bcf3afc483e22c590f3f23c2
Heidrun Heidarsdottir <heidrunh17@ru.is>
2018-10-11 11:26:00 +0000

    Implemented add to return sum of multiple numbers

--- calculator.js
+++ calculator.js
@@ -1,12 +1,16 @@
 function add (number){
-	if(number == "")
-		return 0;
-	if(number.includes(","))
+	if(number == "") //if no number is in the string
+		return 0; //return 0
+	if(number.includes(",")) //if there are 2+ numbers seperated by comma
 	{
 		var numberArray = number.split(",");
-		return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+		var total = 0;
+		for(var i = 0; i < numberArray.length; i++){
+			total += parseInt(numberArray[i]); //get the sum of all the numbers
+		}
+		return total; //return the sum of all the numbers
 	}
-	else
+	else //if there is only 1 number in the string, return the number
 		return parseInt(number);
 
 }
