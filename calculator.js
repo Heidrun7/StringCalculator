f16ad1ca94feb1558911664b0f8d7cd46990f12c
Heidrun Heidarsdottir <heidrunh17@ru.is>
2018-10-11 11:27:25 +0000

    Refactoring, creating function for sum of multiple numbers

--- calculator.js
+++ calculator.js
@@ -4,15 +4,18 @@ function add (number){
 	if(number.includes(",")) //if there are 2+ numbers seperated by comma
 	{
 		var numberArray = number.split(",");
-		var total = 0;
-		for(var i = 0; i < numberArray.length; i++){
-			total += parseInt(numberArray[i]); //get the sum of all the numbers
-		}
-		return total; //return the sum of all the numbers
+		return sum(numberArray);
 	}
 	else //if there is only 1 number in the string, return the number
 		return parseInt(number);
+}
 
+function sum(numberArray){
+	var total = 0;
+	for(var i = 0; i < numberArray.length; i++){
+		total += parseInt(numberArray[i]); //get the sum of all the numbers
+	}
+	return total; //return the sum of all the numbers
 }
 
 module.exports = add;
