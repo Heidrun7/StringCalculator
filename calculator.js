0b4327323a7113b8613c1116b86803cd042cc1d9
Heidrun Heidarsdottir <heidrunh17@ru.is>
2018-10-11 10:49:32 +0000

    Implemented add to return sum of 2 numbers

--- calculator.js
+++ calculator.js
@@ -1,8 +1,14 @@
 function add (number){
 	if(number == "")
 		return 0;
+	if(number.includes(","))
+	{
+		var numberArray = number.split(",");
+		return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+	}
+	else
+		return parseInt(number);
 
-	return parseInt(number);
 }
 
 module.exports = add;
